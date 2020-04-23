9967a8d2b3d56f250c816445a73ae69b1ca91375
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2020-04-23 11:52:46 -0300

    Adicionado aula 06

---
+++ aula06/utils.py
@@ -0,0 +1,25 @@
+from models import Pessoas
+
+def insere():
+    pessoa = Pessoas(nome='Sebastian', idade=298)
+    pessoa.save()
+
+def consulta():
+    pessoa = Pessoas.query.all()
+    print(pessoa)
+    pessoa = Pessoas.query.filter_by(nome='Sebastian').first()
+
+def alterar():
+    pessoa = Pessoas.query.filter_by(nome='Sebastian').first()
+    pessoa.nome = 'Sebastian'
+    pessoa.save()
+
+def excluir():
+    pessoa = Pessoas.query.filter_by(nome='Sebastian').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    # insere()
+    # alterar()
+    excluir()
+    consulta()
